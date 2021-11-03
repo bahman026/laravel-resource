45fb6541a9a8fe0350e5624da2163e96c5c1f05d
bahman <bahman026@gmail.com>
2021-11-03 19:52:05 +0330

    create BResource.php

---
+++ src/BResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace bahman026\laravelResource;
+
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+class BResource
+{
+
+    static function resource($model, $columns, $dataName = null)
+    {
+
+        try {
+
+            $resource = Resource::collection($model);
+
+        } catch (\BadMethodCallException $badMethodCallException) {
+
+            $resource = new Resource($model);
+
+        }
+
+        if ($dataName == null)
+            return $resource->toArray($columns);
+        else
+            return [$dataName => $resource->toArray($columns)];
+
+    }
+}
+
