721509466f63ee304114fae1b67699b3f22b2b19
bahman <bahman026@gmail.com>
2021-11-03 19:48:10 +0330

    create laravelResourceServiceProvider.php

---
+++ src/LaravelResourceServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace bahman026\laravelResource;
+
+use Illuminate\Support\ServiceProvider ;
+
+class laravelResourceServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+
+    }
+
+    public function register()
+    {
+
+    }
+}
+
+?>
